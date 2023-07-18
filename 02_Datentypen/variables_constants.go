9d5cbccf54a5d11c7062ecfbcbc6e76cc67aca34
Jan Stamer <914776+remast@users.noreply.github.com>
2023-07-18 18:14:22 +0200

    Updated zero values example

--- 02_Datentypen/variables_constants.go
+++ 02_Datentypen/variables_constants.go
@@ -3,7 +3,7 @@ package main
 import "fmt"
 
 // Konstante für Package
-const greetingPart1 = "Hello"
+const greetingPart2 = "Hello"
 
 // Variable für Package
 var aPackageVar string = "?"
@@ -14,7 +14,7 @@ func main() {
 	// Deklaration und Zuweisung in langer Form
 	var s1 string = "a string"
 
-	//  Deklaration und Zuweisung in kurzer Form
+	// Deklaration und Zuweisung in kurzer Form
 	s2 := "another string"
 
 	// Deklaration und Zuweisung trennen
@@ -23,9 +23,15 @@ func main() {
 
 	fmt.Println("Variablen:", s1, s2, s3)
 
+	// Vorbelegung mit "leeren" Werten
+	var s0 string
+	var i0 int
+	var b0 bool
+	fmt.Println("Leere Variablen", s0, i0, b0)
+
 	// -- Konstanten
 
 	// Lokale Konstante
-	const greetingPart2 = "Gopher"
+	const greetingPart1 = "Hello"
 	fmt.Println("Konstanten:", greetingPart1, greetingPart2)
 }
